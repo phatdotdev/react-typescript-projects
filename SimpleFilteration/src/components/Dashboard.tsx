7b94bf119a6f3ba9146db0ff66b7af8ff8ec8711
Phat13092004 <dtphat.130904@gmail.com>
2025-08-17 18:00:20 +0700

    first commit

---
+++ SimpleFilteration/src/components/Dashboard.tsx
@@ -0,0 +1,15 @@
+import ProjectTable from "./ProjectTable";
+import Sidebar from "./Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 bg-gray-900">
+        <ProjectTable />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
